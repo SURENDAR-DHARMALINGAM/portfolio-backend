83778063bc61bc50a05fd1fc67cc8ea5d604cdc4
SURENDAR <dharmalingamsure007@gmail.com>
2026-05-11 17:23:12 +0530

    Fixed views.py

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Contact
 from .serializers import ContactSerializer
-from django.core.mail import send_mail
+
 
 @api_view(['POST'])
 def contact_view(request):
@@ -11,22 +12,10 @@ def contact_view(request):
 
     if serializer.is_valid():
 
-        contact = serializer.save()
+        serializer.save()
 
-        send_mail(
-            subject=f"Portfolio Contact from {contact.name}",
-            message=f"""
-Name: {contact.name}
-
-Email: {contact.email}
-
-Message:
-{contact.message}
-""",
-            from_email='dharmalingamsure007@gmail.com',
-            recipient_list=['dharmalingamsure007@gmail.com'],
-        )
-
-        return Response({"message": "Message sent successfully"})
+        return Response({
+            "message": "Message Sent Successfully"
+        })
 
     return Response(serializer.errors, status=400)
