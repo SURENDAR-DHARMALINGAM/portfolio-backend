8a2b6c50bf2f16a40a085e6d0245ca950e827b43
SURENDAR <dharmalingamsure007@gmail.com>
2026-05-12 15:24:59 +0530

    superuser and admin panel created

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Contact
 
-admin.site.register(Contact)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'email', 'created_at')
+
+    search_fields = ('name', 'email')
+
+    list_filter = ('created_at',)
